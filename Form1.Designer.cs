23fb36ff7c97a880b65175020217c54960650cdf
jynso <ecwt26@gmail.com>
2025-03-08 10:15:37 +0800

    initial commit :D

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FinaCore
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginWindow));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelUsername = new System.Windows.Forms.Panel();
+            this.panelPassword = new System.Windows.Forms.Panel();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelUsername.SuspendLayout();
+            this.panelPassword.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(38)))), ((int)(((byte)(34)))));
+            resources.ApplyResources(this.panelMenu, "panelMenu");
+            this.panelMenu.Name = "panelMenu";
+            // 
+            // labelWelcome
+            // 
+            resources.ApplyResources(this.labelWelcome, "labelWelcome");
+            this.labelWelcome.Name = "labelWelcome";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBoxUsername, "textBoxUsername");
+            this.textBoxUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.textBoxUsername.Name = "textBoxUsername";
+            // 
+            // labelPassword
+            // 
+            resources.ApplyResources(this.labelPassword, "labelPassword");
+            this.labelPassword.Name = "labelPassword";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.loginBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.loginBtn, "loginBtn");
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // labelUsername
+            // 
+            resources.ApplyResources(this.labelUsername, "labelUsername");
+            this.labelUsername.Name = "labelUsername";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FloralWhite;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelUsername
+            // 
+            this.panelUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.panelUsername.Controls.Add(this.textBoxUsername);
+            resources.ApplyResources(this.panelUsername, "panelUsername");
+            this.panelUsername.Name = "panelUsername";
+            // 
+            // panelPassword
+            // 
+            this.panelPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.panelPassword.Controls.Add(this.textBoxPassword);
+            resources.ApplyResources(this.panelPassword, "panelPassword");
+            this.panelPassword.Name = "panelPassword";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBoxPassword, "textBoxPassword");
+            this.textBoxPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.textBoxPassword.Name = "textBoxPassword";
+            // 
+            // LoginWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.Controls.Add(this.panelPassword);
+            this.Controls.Add(this.panelUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.panelMenu);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(96)))), ((int)(((byte)(96)))));
+            this.Name = "LoginWindow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelUsername.ResumeLayout(false);
+            this.panelUsername.PerformLayout();
+            this.panelPassword.ResumeLayout(false);
+            this.panelPassword.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelUsername;
+        private System.Windows.Forms.Panel panelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinaCore
+{
+    public partial class LoginWindow : Form
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            roundedBtn(loginBtn, 5);
+
+            roundedPanel(panelUsername, 5);
+            roundedPanel(panelPassword, 5);
+
+            labelWelcome.BringToFront();
+
+
+
+        }
+
+
+
+        private void roundedBtn(Button button, int radius)
+        {
+            //from claude sonnet :D
+            //method for rounded corners sa buttons
+            //pwede remove kung ayaw nyo
+
+            if (button.Height < radius * 2) radius = button.Height / 2;
+            if (button.Width < radius * 2) radius = button.Width / 2;
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(0, 0, radius * 2, radius * 2, 180, 90);
+            path.AddLine(radius, 0, button.Width - radius, 0);
+            path.AddArc(button.Width - radius * 2, 0, radius * 2, radius * 2, 270, 90);
+            path.AddLine(button.Width, radius, button.Width, button.Height - radius);
+            path.AddArc(button.Width - radius * 2, button.Height - radius * 2, radius * 2, radius * 2, 0, 90);
+            path.AddLine(button.Width - radius, button.Height, radius, button.Height);
+            path.AddArc(0, button.Height - radius * 2, radius * 2, radius * 2, 90, 90);
+            path.CloseFigure();
+
+            button.Region = new Region(path);
+
+        }
+
+        private void roundedPanel(Panel panel, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(0, 0, radius * 2, radius * 2, 180, 90);
+            path.AddLine(radius, 0, panel.Width - radius, 0);
+            path.AddArc(panel.Width - radius * 2, 0, radius * 2, radius * 2, 270, 90);
+            path.AddLine(panel.Width, radius, panel.Width, panel.Height - radius);
+            path.AddArc(panel.Width - radius * 2, panel.Height - radius * 2, radius * 2, radius * 2, 0, 90);
+            path.AddLine(panel.Width - radius, panel.Height, radius, panel.Height);
+            path.AddArc(0, panel.Height - radius * 2, radius * 2, radius * 2, 90, 90);
+            path.CloseFigure();
+
+            panel.Region = new Region(path);
+        }
+
+        
+    }
+}
